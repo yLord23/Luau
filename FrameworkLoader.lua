1a8ccf044a51709a642b9257484f26b1060d2bb0
yLord23 <122044543+yLord23@users.noreply.github.com>
2024-02-17 07:45:13 -0300

    Update FrameworkLoader.lua

--- FrameworkLoader.lua
+++ FrameworkLoader.lua
@@ -1,6 +1,7 @@
 --[[
  --> Loads the game structure and returns it in an array.
- --> 12/28/2023
+ --> 15/02/2023
+ --> @yLord <> @yLordGhost
 
 @Framework Library
 ]]--
@@ -11,55 +12,78 @@
 local _Extra = require(script._Extra)
 
 --//Services
-local Players : Players = game:GetService("Players")
+local Players: Players = game:GetService("Players")
+local RunService: RunService = game:GetService("RunService")
 
 --//Variables
 local frameworkFolder = script.Parent
 local DependenciesFolder = frameworkFolder:WaitForChild("Dependencies")
-local clientDependenciesFolder = DependenciesFolder:WaitForChild("Client")
+local Side: string = (RunService:IsServer() and "Server" or "Client")   
 
 --//Types
 type serviceType = {[string]: any} 
-type moduleType = {[string]: {any}?} 
 type dependencieSetter<Key, Value> = {[Key]: Value}
 type loadedDependencie = {[string] : {}}
 
 --//Setup
 local _Services: serviceType = {
-	Players = Players
+	Players = Players,
+	ServerScriptService = Side == "Server" and game:GetService("ServerScriptService")
 }
 
 local Library = {
 	Services = _Services
 }
 
-local function unpackLoadedDependencie(loadedModules: loadedDependencie) : ()
-	--> Unpack loaded modules in main framework table
-	
-	for moduleName: string, userdata: {}? in loadedModules do
-		Library[moduleName] = userdata
-	end
-	
+local function cycleAvoider(): number
+	return RunService.Heartbeat:Wait()
 end
 
-local function compileDependencie(Dependencies: {[number]: Folder}) : () 
+local function unpackLoadedDependencie(loadedModules: loadedDependencie, dependencieClass: string?) : ()
+	if dependencieClass then
+		--> Create a new dependencieClass if none]
+		Library[dependencieClass] = {}
+	end
+	
+	--> Unpack loaded modules in main framework table
+	for moduleName: string, userdata: {}? in loadedModules do
+		if dependencieClass then
+			Library[dependencieClass][moduleName] = userdata
+		else
+			Library[moduleName] = userdata :: any
+		end
+	end
+end
+
+local function compileDependencie(Dependencies: {[number]: Folder}, dependencieClass: string) : () 
+	--> Avoid de module cycle error
+	cycleAvoider() 
+	
 	--> Require and save dependencies modules
-	
 	local loadedModulesFromDependencie: loadedDependencie = {}
-	
+
 	for _, Folder: Folder in Dependencies do
 		local dependencieModules: dependencieSetter<number, ModuleScript> = Folder:GetChildren() :: {}
-		
+
 		for _,moduleScript: ModuleScript in dependencieModules do
 			local Return = require(moduleScript) :: any
 			loadedModulesFromDependencie[moduleScript.Name] = Return :: typeof(Return)
 		end
 	end
-	
-	unpackLoadedDependencie(loadedModulesFromDependencie)
+
+	unpackLoadedDependencie(loadedModulesFromDependencie, dependencieClass)
 end
 
-compileDependencie(clientDependenciesFolder:GetChildren())
-unpackLoadedDependencie(_Extra)
+local function setupFramework()
+	--> Call loaders & compilers
+	compileDependencie(Side == "Client" and DependenciesFolder:WaitForChild("Client"):GetChildren() or _Services.ServerScriptService:WaitForChild("Server"):GetChildren(), Side)
+	compileDependencie(DependenciesFolder:WaitForChild("Shared"):GetChildren(), "Shared")
+	unpackLoadedDependencie(_Extra)
+	
+	--> Setup Completed > Framework loaded
+	Library.isFrameworkLoaded = true
+end
+
+coroutine.wrap(setupFramework)()
 
 return Library
